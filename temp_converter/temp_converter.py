a3e1572787ab0e492087e6183a6d787f0f7660af
manuelevasquez <mev16@sunyorange.edu>
2024-09-16 19:02:25 -0400

    Update temp_converter.py

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -1,12 +1,8 @@
+
 #!/usr/bin/python3
 
-# process
-def c2f(c):
-    return c * 9 / 5 + 32
-
-def main(cel):
-    return c2f(cel)
-
-if __name__ == "__main__":
-    cel = 100         # input
-    print(main(cel))  # output
+    farenheit =int (input("Farenheit to Celsius, type the Farenheit:"))
+    celsius =(farenheit-32)*5//9
+    print(celsius)
+    #This was coded this way to know the variable of the temperature in diffrent locations.
+    
